c11e4477f04d6db67d61eb308b6f65a3ce6877ab
Kevin Kee <kevinkee9@gmail.com>
2021-02-02 13:52:31 +0900

    routes: GET /, add views and index page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const app = require("./server");
 require("./routes");
 
+app.set("view engine", "pug");
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running at https://localhost:${port}`);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,28 @@
+const app = require("../server");
 const routeCompanies = require("./companies");
 const routeRecruiters = require("./recruiters");
 
+const index = app.get("/", async (_, res) => {
+  const routeList = [];
+  app._router.stack.forEach((r) => {
+    if (r.route && r.route.path) {
+      const methods = Object.keys(r.route.methods);
+      routeList.push(methods.join(", ").toUpperCase() + ": " + r.route.path);
+    }
+  });
+
+  res.render("index", {
+    title: "Recruit API",
+    header: "Recruit API",
+    gitMessage: "View this project on Github:",
+    gitLink: "https://github.com/friendofdog/cc17-recruit-api",
+    routeText: "List of routes:",
+    routeList: routeList
+  });
+});
+
 module.exports = [
+  index,
   routeCompanies,
   routeRecruiters
 ];
